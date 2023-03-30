2056d665c71d9a35b4fcc220c4a47abc4dff7885
Jonathan Delgado <hi@jon.soy>
2023-03-30 12:23:33 -0300

    fix: upgrade types

--- types.d.ts
+++ types.d.ts
@@ -6,6 +6,10 @@ declare module "icalendar.js/ast" {
     export * from "./dist/ast";
 }
 
+declare module "icalendar.js/property_types" {
+    export * from "./dist/property_types";
+}
+
 declare module "icalendar.js" {
     export * from "./dist/icalendar";
 }
